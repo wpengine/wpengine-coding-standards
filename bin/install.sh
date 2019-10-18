52c7e7d543866338b9f436c55dc609c38df8560d
Steven K Word <stevenword@gmail.com>
2019-10-17 19:36:38 -0500

    Update for consumption

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo -e "Hello Word\n"
+VENDOR_BIN := ./vendor/bin
+
+@ $(VENDOR_BIN)/phpcs --config-set default_standard WP-Engine
+@ $(VENDOR_BIN)/phpcs --config-set show_progress 1 > /dev/null 2>&1
+@ $(VENDOR_BIN)/phpcs --config-set colors 1 > /dev/null 2>&1
+@ $(VENDOR_BIN)/phpcs -i; echo "\n"
+@echo "USAGE: wpecs [--strict] [phpcs-options] <path>\n"
+@echo "For assistance getting started try 'wpecs -h'\n"
